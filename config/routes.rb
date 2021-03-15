f954acbb8f66495bbaa1bf07cfd945c9b4637802
hiyu3298 <akamatsu11218991@gmail.com>
2021-03-15 09:11:24 +0000

    [Update] routesを更新

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,24 @@ devise_for :customers, controllers: {
   registrations: 'customers/registrations'
 }
 
+  namespace :admin do
+    root 'homes#top'
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :update, :edit]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
   scope module: 'public' do
     root 'homes#top'
+    resources :orders, only: [:new, :index, :show]
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update]
+    resources :cart_items, only: [:index, :update, :create]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
    end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
