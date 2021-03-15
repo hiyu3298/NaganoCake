d1729fd1bb25f9c5d270c0a3c62b870936878eb2
hiyu3298 <akamatsu11218991@gmail.com>
2021-03-15 05:57:00 +0000

    [Add] sign upのviewを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_082320) do
+ActiveRecord::Schema.define(version: 2021_03_15_054030) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2021_03_14_082320) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "telephone_number"
+    t.boolean "is_active", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
