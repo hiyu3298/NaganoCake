2bc27f2050f9edb19f6877c43e2eb2426e7e241f
hiyu3298 <akamatsu11218991@gmail.com>
2021-03-16 05:28:41 +0000

    [Add] 商品登録・編集機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  attachment :image
+
+  enum is_active: { on_sale: 0, off_sale: 1 }
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd56d13c8281959d852c7d0c87648887b878e8d5eeb34869627d4cfa0606afe2dc6123f0b8a0b055d0ea114ed89587cc11389d6b3ae967420d4578994fee702da'
