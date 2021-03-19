3ef84d64855a4666154e3b7e138073ed8a013584
hiyu3298 <akamatsu11218991@gmail.com>
2021-03-19 06:18:50 +0000

    [Add] mypageの追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,32 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @user = current_customer
+  end
+
+  def edit
+    @user = current_customer
+  end
+
+  def update
+    @user = current_customer
+    if @user.update(customer_params)
+      redirect_to customer_path
+    else
+      render :exit
+    end
+  end
+
+  def delete_verification
+  end
+
+  def destroy
+    user = Customer.find(params[:id])
+    user.destroy
+    redirect_to root_path
+  end
+
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_active, :email)
+  end
 end
